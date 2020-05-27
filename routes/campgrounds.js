037d0ded5fa889973eca5d55c867ae6897de9e79
Yan Waipann <yanwaipann@gmail.com>
2020-05-27 21:32:47 +0630

    Update callback to promise

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,96 +3,115 @@ const router = express.Router();
 const Campground = require("../models/campground");
 const middleWare = require("../middlewares");
 
-router.get("/", (req, res) => {
-  Campground.find({}, (err, allCampGrounds) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("campground/campgrounds", {
-        title: "CampGrounds",
-        data: allCampGrounds,
-      });
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    const allCampGrounds = await Campground.find({});
+    res.render("campground/campgrounds", {
+      title: "CampGrounds",
+      data: allCampGrounds,
+    });
+  } catch (error) {
+    console.log(error.message);
+    req.flash("error", "Something went wrong on our server!");
+    res.redirect("/");
+  }
 });
 
-router.post("/", middleWare.isLoggedIn, (req, res) => {
-  let campName = req.body.name;
-  let imgUrl = req.body.url;
-  let description = req.body.description;
-  let location = req.body.location;
-  let author = {
-    id: req.user._id,
-    username: req.user.username,
-  };
-  let newCampGroundData = {
-    name: campName,
-    imageUrl: imgUrl,
-    description: description,
-    location: location,
-    author: author,
+router.post("/", middleWare.isLoggedIn, async (req, res) => {
+  const { campName, url, description, location } = req.body;
+  const { _id, username } = req.user;
+
+  const author = {
+    id: _id,
+    username: username,
   };
 
-  Campground.create(newCampGroundData, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect("/campgrounds");
-    }
-  });
+  try {
+    const addedCampInfo = await Campground.create({
+      name: campName,
+      imageUrl: url,
+      description: description,
+      location: location.replace(",", " "),
+      author: author,
+    });
+
+    console.log("Added camp: " + addedCampInfo._id);
+    res.redirect("/campgrounds");
+  } catch (error) {
+    console.log(error.message);
+    req.flash("error", "Something went wrong on our server!");
+    res.redirect("/campgrounds");
+  }
 });
 
 router.get("/new", middleWare.isLoggedIn, (req, res) => {
   res.render("campground/addNewCamp", { title: "Add New Camp" });
 });
 
-router.get("/:id", (req, res) => {
-  let id = req.params.id;
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
 
-  Campground.findById(id)
-    .populate("comments")
-    .exec((err, foundCampGround) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("campground/campGroundInfo", {
-          title: "CampDetails",
-          data: foundCampGround,
-        });
-      }
+  try {
+    const foundCampGround = await Campground.findById(id)
+      .populate("comments")
+      .exec();
+    res.render("campground/campGroundInfo", {
+      title: "CampDetails",
+      data: foundCampGround,
     });
+  } catch (error) {
+    console.log(error.message);
+    req.flash("error", "Something went wrong on our server!");
+    res.redirect("/campgrounds");
+  }
 });
 
 // Edit and Update Routes
-router.get("/:id/edit", middleWare.checkCampgroundOwnership, (req, res) => {
-  Campground.findById(req.params.id, (err, foundCampground) => {
-    if (err) console.log(err);
-    else {
+router.get(
+  "/:id/edit",
+  middleWare.checkCampgroundOwnership,
+  async (req, res) => {
+    try {
+      const camp = await Campground.findById(req.params.id);
       res.render("campground/editCamp", {
-        title: `Edit ${foundCampground.name}`,
-        data: foundCampground,
+        title: `Edit ${camp.name}`,
+        data: camp,
       });
+    } catch (error) {
+      console.log(error.message);
+      req.flash("error", "Something went wrong on our server!");
+      res.redirect("/campgrounds");
     }
-  });
+  }
+);
+
+router.put("/:id", middleWare.checkCampgroundOwnership, async (req, res) => {
+  const { campName, imageUrl, description, location } = req.body;
+
+  try {
+    const updatedCamp = await Campground.findByIdAndUpdate(req.params.id, {
+      name: campName,
+      imageUrl: imageUrl,
+      description: description,
+      location: location.replace(",", " "),
+    });
+    res.redirect("/campgrounds/" + updatedCamp._id);
+  } catch (error) {
+    console.log(error.message);
+    req.flash("error", "Something went wrong on our server!");
+    res.redirect("/campgrounds");
+  }
 });
 
-router.put("/:id", middleWare.checkCampgroundOwnership, (req, res) => {
-  let updateData = req.body.campground;
-  Campground.findByIdAndUpdate(req.params.id, updateData, (err, data) => {
-    if (err) console.log(err);
-    else {
-      res.redirect("/campgrounds/" + data._id);
-    }
-  });
-});
-
-// Destroy Route
-
-router.delete("/:id", middleWare.checkCampgroundOwnership, (req, res) => {
-  Campground.findByIdAndRemove(req.params.id, (err) => {
-    if (err) console.log(err);
-    else res.redirect("/campgrounds");
-  });
+router.delete("/:id", middleWare.checkCampgroundOwnership, async (req, res) => {
+  try {
+    await Campground.findByIdAndDelete(req.params.id);
+    res.redirect("/campgrounds");
+  } catch (error) {
+    console.log(error.message);
+    req.flash("error", "Something went wrong on our server!");
+    res.redirect("/campgrounds");
+  }
 });
 
 module.exports = router;
